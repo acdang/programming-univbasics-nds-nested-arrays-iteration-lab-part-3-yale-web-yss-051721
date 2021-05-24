ba95060083f3c15f7ebb30de87422c304480ccae
ambitious-caller-8426 <anhcdang@gmail.com>
2021-05-24 20:00:46 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,6 +3,7 @@ def join_nested_strings(src)
   # Combine all Strings present in the AoA into a single value and return it
   row = 0
   while row < src.count do
+    element = 0
     row += 1
   end
 end
